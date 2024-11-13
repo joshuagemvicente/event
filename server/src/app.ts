ba1d8ef3809a9d77544c0c9643cf5a2030286537
joshuagemvicente6@gmail.com <joshuagemvicente6@gmail.com>
2024-11-13 10:35:39 +0800

    added helmet and morgan

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application } from "express";
 import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
 
 export const app: Application = express();
 
 app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
